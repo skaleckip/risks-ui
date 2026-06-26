1ffdfaa24d4b12134f21c5779c8d9855f2f47a55
Przemysław Skałecki <skaleckip@gmail.com>
2026-06-26 08:38:12 +0200

    Make Keycloak Url changeable with env variable.

--- app/keycloak.ts
+++ app/keycloak.ts
@@ -2,7 +2,7 @@ import Keycloak from 'keycloak-js'
 
 // noinspection SpellCheckingInspection
 const keycloak = new Keycloak({
-  url: 'http://localhost:9090',
+  url: import.meta.env.KEYCLOAK_URL ?? 'http://localhost:9090',
   realm: 'norman',
   clientId: 'risks-ui',
 })
